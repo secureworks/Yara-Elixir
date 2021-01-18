d77d6b2ec5a7c2bf63077faa405508b83caef406
Nick Bargnesi <nbargnesi@users.noreply.github.com>
2021-01-17 23:28:03 -0500

    pull custom compiler tasks from Mix

--- mix.exs
+++ mix.exs
@@ -1,16 +1,3 @@
-defmodule Mix.Tasks.Compile.YaraElixir do
-
-  @doc """
-  Returns compiler definitions depending on `Mix.env()/0`.
-  """
-  def env_args() do
-    cond do
-      Mix.env() == :dev -> ["-DCHATTY"]
-      true -> []
-    end
-  end
-end
-
 defmodule YaraElixir.MixProject do
   use Mix.Project
 
@@ -20,8 +7,7 @@ defmodule YaraElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      compilers: [:yara_elixir] ++ Mix.compilers()
+      deps: deps()
     ]
   end
 
