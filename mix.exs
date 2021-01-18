1fd7893adaee392d41f81cf95551cf6327e1a06b
Nick Bargnesi <nbargnesi@users.noreply.github.com>
2021-01-17 23:21:17 -0500

    switch to CMake

--- mix.exs
+++ mix.exs
@@ -1,28 +1,4 @@
 defmodule Mix.Tasks.Compile.YaraElixir do
-  @doc """
-  Returns GCC arguments depending on `:os.type/0`.
-  """
-  def os_args() do
-    if elem(:os.type(), 1) == :darwin do
-      [
-        "-fPIC",
-        "-flat_namespace",
-        "-undefined",
-        "suppress",
-        "-I/usr/local/Cellar/erlang/23.2.1/lib/erlang/usr/include",
-        "-L/usr/local/Cellar/erlang/23.2.1/lib/erlang/usr/lib",
-        "-lyara",
-        "-L/usr/local/Cellar/yara/4.0.2/lib"
-      ]
-    else
-      [
-        "-fPIC",
-        "-shared",
-        "-I/usr/lib/erlang/usr/include",
-        "-lyara"
-      ]
-    end
-  end
 
   @doc """
   Returns compiler definitions depending on `Mix.env()/0`.
@@ -32,13 +8,6 @@ defmodule Mix.Tasks.Compile.YaraElixir do
       Mix.env() == :dev -> ["-DCHATTY"]
       true -> []
     end
-  end
-
-  def run(_args) do
-    gcc_args = os_args() ++ env_args()
-    cmd_args = gcc_args ++ ["shared/yara_elixir.c", "-o", "yara_elixir.so"]
-    {result, _errcode} = System.cmd("gcc", cmd_args, stderr_to_stdout: true)
-    IO.puts(result)
   end
 end
 
@@ -66,8 +35,6 @@ defmodule YaraElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
